31f169f62ba92c62d0b0ee1b0eb82a6ad2bbf098
Shreyas Raorane <raorane@seas.upenn.edu>
2025-03-03 19:16:58 -0500

    added config and code to handle the config file, next step is to add lookahead distance and throttle value

--- src/wall_follow_ui_control/scripts/wall_follow_get_params.py
+++ src/wall_follow_ui_control/scripts/wall_follow_get_params.py
@@ -4,7 +4,11 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
 
+import json
+
 from roboracer_interfaces.msg import CarControlWallFollow
+
+from ament_index_python.packages import get_package_share_directory
 
 class WallFollowGetParams(Node):
     def __init__(self):
@@ -15,15 +19,32 @@ class WallFollowGetParams(Node):
 
         self.wall_follow_ui_control_publisher_ = self.create_publisher(CarControlWallFollow, "wall_follow_params", qos_profile)
 
-        self.wall_follow_ui_control_timer_ = self.create_timer(30.0, self.publish_wall_follow_ui_control)
+        self.wall_follow_ui_control_timer_ = self.create_timer(10.0, self.publish_wall_follow_ui_control)
     
     def publish_wall_follow_ui_control(self):
         wall_follow_ui_control_msg = CarControlWallFollow()
-        wall_follow_ui_control_msg.kp = 2.4
-        wall_follow_ui_control_msg.kd = 1.0
-        wall_follow_ui_control_msg.ki = 0.0
+        # wall_follow_ui_control_msg.kp = 2.4
+        # wall_follow_ui_control_msg.kd = 1.0
+        # wall_follow_ui_control_msg.ki = 0.0
 
-        self.wall_follow_ui_control_publisher_.publish(wall_follow_ui_control_msg)
+        package_share_dir = get_package_share_directory("wall_follow_ui_control")
+        absolute_file_path = package_share_dir + "/config/wall_follow_params.json"
+
+        with open(absolute_file_path, 'r') as file:
+            config_data = json.load(file)
+
+
+            wall_follow_ui_control_msg.kp = float(config_data["kp"])
+            wall_follow_ui_control_msg.kd = float(config_data["kd"])
+            wall_follow_ui_control_msg.ki = float(config_data["ki"])
+
+            self.get_logger().info(f'kp: {config_data["kp"]}')
+            self.get_logger().info(f'kd: {config_data["kd"]}')
+            self.get_logger().info(f'ki: {config_data["ki"]}')
+
+            self.wall_follow_ui_control_publisher_.publish(wall_follow_ui_control_msg)
+
+            # {'throttle': '0.1', 'lookahead_dist': '0.8', 'kp': '0.8', 'kd': '4.2', 'ki': '1.248', 'flag_reason': 'Latest', 'flag_msg': '213123'}
 
 def main(args=None):
     rclpy.init(args=args)
