ab96171daa8f2f43aad0ae2e8ff5f23e88856ae3
Shreyas Raorane <raorane@seas.upenn.edu>
2025-03-09 22:10:06 -0400

    subscriber added to get updated parametes accordingly

--- src/gap_follow_ui_control/scripts/gap_follow_ui_control_node.py
+++ src/gap_follow_ui_control/scripts/gap_follow_ui_control_node.py
@@ -2,11 +2,44 @@
 
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile
+
+from roboracer_interfaces.msg import CarControlGapFollow
+from std_msgs.msg import Float32
+from sensor_msgs.msg import LaserScan
+
+import numpy as np
 
 class GapFollowUIControlNode(Node):
     def __init__(self):
         super().__init__("gap_follow_ui_control_node")
         self.get_logger().info("Gap Follow UI Control Node Started")
+
+        self.throttle = 0.2
+        self.window_half_size = 5
+        self.disparity_extender = 5
+        self.max_actionable_dist = 2.0
+
+        gap_follow_params_sub_topic = "gap_follow_params"
+
+        qos_profile = QoSProfile(depth=10)
+
+        # Subscriber Topics
+        self.gap_follow_params_sub_ = self.create_subscription(CarControlGapFollow, gap_follow_params_sub_topic, self.gap_follow_params_callback, qos_profile)
+
+    def gap_follow_params_callback(self, msg):
+        if self.throttle != msg.throttle:
+            self.throttle = msg.throttle 
+
+        if self.window_half_size != msg.window_half_size:
+            self.window_half_size = msg.window_half_size
+
+        if self.disparity_extender != msg.disparity_extender:
+            self.disparity_extender = msg.disparity_extender
+
+        if self.max_actionable_dist != msg.max_actionable_dist:
+            self.max_actionable_dist = msg.max_actionable_dist
+
 
 def main(args=None):
     rclpy.init(args=args)
