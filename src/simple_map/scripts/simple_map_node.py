5193034b4af6fce3a299e5a731e510873285c072
Shreyas Raorane <raorane@seas.upenn.edu>
2025-06-03 01:59:52 -0400

    occupancy grid created

--- src/simple_map/scripts/simple_map_node.py
+++ src/simple_map/scripts/simple_map_node.py
@@ -1,12 +1,150 @@
 #!/usr/bin/env python3
 
+import numpy as np
+from transforms3d.euler import quat2euler
+
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSDurabilityPolicy
+
+from geometry_msgs.msg import Point
+from sensor_msgs.msg import Imu, LaserScan
+from nav_msgs.msg import OccupancyGrid
+
+from simple_map.utils import get_grid_coordinates, get_world_coordinates, update_grid_with_ray
 
 class SimpleMapNode(Node):
     def __init__(self):
         super().__init__("simple_map_node")
         self.get_logger().info("Simple Map Node from Python has been started")
+
+        self.declare_parameter('ips_topic', '/autodrive/f1tenth_1/ips')
+        self.declare_parameter('imu_topic', '/autodrive/f1tenth_1/imu')
+        self.declare_parameter('scan_topic', '/autodrive/f1tenth_1/lidar')
+
+        self.declare_parameter("map_topic", "/map")
+
+        self.declare_parameter('map_height', 100)
+        self.declare_parameter('map_width', 100)
+        self.declare_parameter('map_resolution', 0.1)
+        self.declare_parameter('map_origin', (0.0, 0.0))
+
+        self.declare_parameter('expand_occ_size', 2)
+
+        # Subscribe to topics
+        ips_topic = self.get_parameter('ips_topic').get_parameter_value().string_value
+        imu_topic = self.get_parameter('imu_topic').get_parameter_value().string_value
+        scan_topic = self.get_parameter('scan_topic').get_parameter_value().string_value
+        
+        # Publish map topic
+        map_topic = self.get_parameter('map_topic').get_parameter_value().string_value
+
+        # Map parameters
+        self.map_height = self.get_parameter('map_height').get_parameter_value().integer_value
+        self.map_width = self.get_parameter('map_width').get_parameter_value().integer_value
+        self.map_resolution = self.get_parameter('map_resolution').get_parameter_value().double_value
+        self.map_origin = self.get_parameter('map_origin').get_parameter_value().double_array_value
+
+        self.expand_occ_size = self.get_parameter('expand_occ_size').get_parameter_value().integer_value
+
+        # Default Occupancy Grid
+        self.occupancy_grid = np.array(
+            np.zeros((self.map_height, self.map_width), dtype=np.int8),
+            dtype=np.int8
+        )
+
+
+        # QoS profile for subscribers
+        qos_profile_sub = QoSProfile(depth=10)
+
+        # Subscribers
+        self.ips_subscriber = self.create_subscription(Point, ips_topic, self.ips_callback, qos_profile_sub)
+        self.imu_subscriber = self.create_subscription(Imu, imu_topic, self.imu_callback, qos_profile_sub)
+        self.scan_subscriber = self.create_subscription(LaserScan, scan_topic, self.scan_callback, qos_profile_sub)
+
+        # QoS profile for publishers
+        qos_profile_pub = QoSProfile(
+            reliability=QoSReliabilityPolicy.RELIABLE,
+            durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
+            depth=10
+        )
+
+        # Publisher for map
+        self.map_publisher = self.create_publisher(OccupancyGrid, map_topic, qos_profile_pub)
+
+        
+
+    def ips_callback(self, msg):
+        # self.get_logger().info(f"Received IPS data: {msg.x}, {msg.y}, {msg.z}", throttle_duration_sec=1.0)
+        self.cur_pos = np.array([msg.x, msg.y, msg.z])
+
+    def imu_callback(self, msg):
+        # self.get_logger().info(f"Received IMU data: {msg}", throttle_duration_sec=1.0)
+
+        self.cur_yaw = quat2euler([msg.orientation.w,
+                                    msg.orientation.x,
+                                    msg.orientation.y,
+                                    msg.orientation.z,
+                                    ])[2]
+
+    def scan_callback(self, scan_msg):
+        # self.get_logger().info(f"Received LaserScan data: {msg.ranges}", throttle_duration_sec=1.0)
+        
+        if not hasattr(self, 'cur_pos') or not hasattr(self, 'cur_yaw'):
+            self.get_logger().warn("Current position or yaw not set yet. Waiting for pose data...")
+            return
+        
+        scan_data = np.array(scan_msg.ranges)
+
+        cur_grid_x, cur_grid_y = get_grid_coordinates(self.cur_pos[0], self.cur_pos[1], self.map_origin, self.map_resolution)
+
+        cur_grid_x = np.clip(cur_grid_x, 0, self.map_width - 1)
+        cur_grid_y = np.clip(cur_grid_y, 0, self.map_height - 1)
+
+        updated_occupancy_grid = np.copy(self.occupancy_grid)
+
+        for i, distance in enumerate(scan_data):
+            if distance == float('inf'):
+                distance = scan_msg.range_max
+
+            angle = scan_msg.angle_min + i * scan_msg.angle_increment
+            
+            angle_world = angle + self.cur_yaw
+
+            updated_occupancy_grid = update_grid_with_ray(
+                updated_occupancy_grid,
+                cur_grid_x,
+                cur_grid_y,
+                angle_world,
+                distance,
+                self.map_resolution,
+                self.map_width,
+                self.map_height,
+                area_size=self.expand_occ_size
+            )
+        
+        self.occupancy_grid = updated_occupancy_grid
+
+        # Publish the occupancy grid
+        self.publish_occupancy_grid()
+
+    def publish_occupancy_grid(self):
+        # Create and publish the updated occupancy grid
+        occupancy_grid_msg = OccupancyGrid()
+        occupancy_grid_msg.header.stamp = self.get_clock().now().to_msg()
+        occupancy_grid_msg.header.frame_id = "map"
+
+        occupancy_grid_msg.info.height = self.map_height
+        occupancy_grid_msg.info.width = self.map_width
+        occupancy_grid_msg.info.resolution = self.map_resolution
+        occupancy_grid_msg.info.origin.position.x = self.map_origin[0]
+        occupancy_grid_msg.info.origin.position.y = self.map_origin[1]
+
+        occupancy_grid_msg.data = self.occupancy_grid.flatten().astype(np.int8).tolist()
+
+        # Publish the updated occupancy grid
+        self.map_publisher.publish(occupancy_grid_msg)
+
 
 def main(args=None):
     rclpy.init(args=args)
